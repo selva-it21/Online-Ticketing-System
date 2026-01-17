3cde7f320dc204ecb255f38fa81bb1beadb43dbc
selva-it21 <selva.it21@gmail.com>
2026-01-17 21:35:23 +0530

    API file added

--- TicketRaisingWebApi/obj/Debug/net10.0/TicketRaisingWebApi.AssemblyInfo.cs
+++ TicketRaisingWebApi/obj/Debug/net10.0/TicketRaisingWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketRaisingWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70b4e628a120c1fe3d13e7dde3400aaf5595fc95")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketRaisingWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketRaisingWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
