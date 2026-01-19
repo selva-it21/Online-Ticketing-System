2e05d1987aca958e8d6d5167586ef37a4e26aaba
Nivetha <nivetha.kumarasamy@gds.ey.com>
2026-01-19 11:40:05 +0530

    EFSLARepository updated

--- backend/TicketRaisingLibrary/Repos/EFSLARepository.cs
+++ backend/TicketRaisingLibrary/Repos/EFSLARepository.cs
@@ -1,8 +1,78 @@
-using System;
-
-namespace TicketRaisingLibrary.Repos;
-
-public class EFSLARepository
-{
-
-}
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using TicketRaisingLibrary.Models;
+using TicketRaisingLibrary.Repos;
+    public class EFSLARepository : ISLARepository
+    {
+        TicketPortalDBContext  context = new TicketPortalDBContext ();
+ 
+        // 1. Get all SLAs
+        public async Task<List<SLA>> GetAllSLAsAsync()
+        {
+            return await context.SLAs.ToListAsync();
+        }
+ 
+        // 2. Get SLA by Id
+        public async Task<SLA> GetSLAAsync(string slaId)
+        {
+            try{
+                SLA sla = await context.SLAs.FirstAsync(s => s.SLAId == slaId);
+                return sla;
+            }
+            catch{
+                throw new TicketingException("No such SLA Id");
+            }
+        }
+ 
+        // 3. Add new SLA
+        public async Task AddSLAAsync(SLA sla)
+        {
+            try{
+                await context.SLAs.AddAsync(sla);
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateException ex) {
+                SqlException sqlException = ex.InnerException as SqlException;
+                if (sqlException != null && sqlException.Number == 2627)
+                    throw new TicketingException("SLA ID already exists");
+                else
+                    throw new TicketingException(sqlException?.Message ?? "Database Error");
+            }
+        }
+ 
+        // 4. Update SLA
+        public async Task UpdateSLAAsync(string slaId, SLA sla)
+        {
+            SLA sla2edit = await GetSLAAsync(slaId);
+ 
+            try {
+                sla2edit.SLAName = sla.SLAName;
+                sla2edit.Priority = sla.Priority;
+                sla2edit.ResponseTime = sla.ResponseTime;
+                sla2edit.ResolutionHours = sla.ResolutionHours;
+ 
+                await context.SaveChangesAsync();
+            }
+            catch (Exception ex){
+                SqlException sqlException = ex.InnerException as SqlException;
+                throw new TicketingException(sqlException?.Message ?? ex.Message);
+            }
+        }
+ 
+        // 5. Delete SLA
+        public async Task DeleteSLAAsync(string slaId)
+        {
+            SLA sla2del = await context.SLAs.Include(s => s.TicketTypes).FirstOrDefaultAsync(s => s.SLAId == slaId);
+ 
+            if (sla2del == null)
+                throw new TicketingException("No such SLA Id");
+ 
+            if (sla2del.TicketTypes.Count == 0) {
+                context.SLAs.Remove(sla2del);
+                await context.SaveChangesAsync();
+            }
+            else{
+                throw new TicketingException("Cannot delete SLA as it is linked to Ticket Types");
+            }
+        }
+    }
