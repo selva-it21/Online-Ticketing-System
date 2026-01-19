bd87a21c21eb12adc995308c8a3dda4c8c41b596
susi2004 <susidharann6@gmial.com>
2026-01-19 09:56:24 +0530

    Added emp repo

--- backend/TicketRaisingLibrary/Repos/EFTicketRepository.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace TicketRaisingLibrary.Repos;
-
-public class EFTicketsRepository
-{
-
-}
--- backend/TicketRaisingLibrary/Repos/TicketingException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace TicketRaisingLibrary.Repos;
-
-public class TicketingException : Exception
-{
-    public TicketingException(string str) : base(str) {}
-}
