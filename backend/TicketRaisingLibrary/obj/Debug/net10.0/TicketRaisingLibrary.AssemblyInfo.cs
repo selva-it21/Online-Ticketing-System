23029d1bffa0aea3b66bd59c81ff6a3dbd0a7d06
user17 <roganth001@gmail.com>
2026-01-20 17:33:34 +0530

    update

--- backend/TicketRaisingLibrary/obj/Debug/net10.0/TicketRaisingLibrary.AssemblyInfo.cs
+++ backend/TicketRaisingLibrary/obj/Debug/net10.0/TicketRaisingLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketRaisingLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+350a8a5ac407f6c4b2f07d1c58943353bfb8679e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d29aa89f510a67d1d731303a3c81ee22c6989a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketRaisingLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketRaisingLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
