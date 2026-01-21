6449a6cbd6cbd80e8da75cdbed5434b0d7af6a6c
susi2004 <susidharann6@gmial.com>
2026-01-21 09:35:04 +0530

    pulled to my branch dev

--- backend/TicketRaisingWebApi/obj/Debug/net10.0/TicketRaisingWebApi.AssemblyInfo.cs
+++ backend/TicketRaisingWebApi/obj/Debug/net10.0/TicketRaisingWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketRaisingWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56b9d7f9b4de285723edc88cd11cb98e6c25e0cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ef490eafb415c0bff152df830c38ea99773132e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketRaisingWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketRaisingWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
