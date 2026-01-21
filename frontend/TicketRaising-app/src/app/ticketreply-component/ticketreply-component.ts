c2f2c3c42c21da1d4b68b1ed3e1e6af41157d595
user17 <roganth001@gmail.com>
2026-01-21 13:03:18 +0530

    TicketReply updated

--- frontend/TicketRaising-app/src/app/ticketreply-component/ticketreply-component.ts
+++ frontend/TicketRaising-app/src/app/ticketreply-component/ticketreply-component.ts
@@ -17,7 +17,9 @@ export class TicketReplyComponent {
   replySvc: TicketReplyService = inject(TicketReplyService);
   ticketSvc: TicketService = inject(TicketService);
   tickets: Ticket[];
-  reply!: TicketReply;
+  replier : string;
+  ticket : Ticket;
+  reply: TicketReply;
   replies: TicketReply[] = [];
   errMsg: string = '';
 
@@ -26,18 +28,44 @@ export class TicketReplyComponent {
   creator : any = sessionStorage.getItem("empId");
   constructor() {
     this.tickets = [];
+    this.ticket  = new Ticket("","","","",new Date() ,"","" ,"")
+    this.replier = "";
+    this.reply= new TicketReply('', '', '', this.ticket.createdByEmpId, this.ticket.assignedToEmpId);
     this.newReply();
     this.showAllReplies();
     this.showAllTickets();
   }
 
+  onTicketIdChange(ticketId : string){
+
+    this.ticketSvc.getOneTicket(ticketId).subscribe({
+      next: (response: Ticket) => {
+        this.ticket = response;
+        //  this.newReply()
+        this.reply.replyByAssignedEmpId = this.ticket.assignedToEmpId;
+        this.reply.replyByCreatorEmpId = this.ticket.createdByEmpId
+        console.log(this.ticket);
+        console.log(this.ticket.createdByEmpId);
+        
+        this.errMsg = '';
+      },
+      error: (err) => {
+        this.errMsg = err.error;
+        console.log(err);
+      }
+    });
+    // this.newReply()
+
+  }
   newReply() {
-    this.reply = new TicketReply('', '', '', this.creator, '');
+       this.reply= new TicketReply('',"" , '', "", "");
+
   }
     showAllTickets(): void {
     this.ticketSvc.showAllTickets().subscribe({
       next: (response: Ticket[]) => {
         this.tickets = response;
+        
         this.errMsg = '';
       },
       error: (err) => {
